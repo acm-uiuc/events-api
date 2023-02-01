3cd171d9b09e06c0d3f63322a4a8079034b194ce
Dev Singh <dsingh14@illinois.edu>
2023-01-31 18:30:17 -0600

    base start project

---
+++ src/index.ts
@@ -0,0 +1,36 @@
+import * as dotenv from 'dotenv'
+dotenv.config()
+import express = require('express');
+import { Request, Response } from "express";
+import mongoose = require('mongoose');
+
+const app = express();
+
+// mongoose.connect(process.env.MONGO_URI!);
+
+app.use(express.json());
+
+interface EventsRequest extends Request {
+    token: string,
+    headers: Record<string, string>,
+  }
+
+const checkToken = (req: EventsRequest, res: Response, next: any) => {
+  const header = req.headers['authorization'];
+  if(typeof header !== 'undefined') {
+    const bearer = header.split(' ');
+    const token = bearer[1];
+    req.token = token;
+    next();
+  } else {
+    res.status(403).send("Not authorized.");
+  }
+};
+
+app.get('/heartbeat', (req: Request, res: Response) => {
+  res.send("Up.");
+})
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server running at http://localhost:${process.env.PORT}`);
+});
