9f516a35bae3cca62f5180caca6e4887a76373c7
Dev Singh <dsingh14@illinois.edu>
2023-03-14 18:38:41 -0500

    fix build script

--- build-arm.sh
+++ build-arm.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 # see: https://docs.docker.com/build/building/multi-platform/
-docker buildx build --platform linux/arm64 -t ghcr.io/acm-uiuc/events-api:latest .
-docker buildx build --platform linux/amd64 -t ghcr.io/acm-uiuc/events-api:latest .
+docker buildx create --name mycustombuilder --driver docker-container --bootstrap
+docker buildx use mycustombuilder
+docker buildx build --platform linux/amd64,linux/arm64 -t ghcr.io/acm-uiuc/events-api:latest --push .
